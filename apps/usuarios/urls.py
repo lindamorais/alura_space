202caf0c8b9b9085d95d6cb4ee06a5582cbb5a9f
Linda <lindaizamorais@gmail.com>
2023-06-01 15:57:14 -0300

    atualizacao alura-space

---
+++ apps/usuarios/urls.py
@@ -0,0 +1,10 @@
+# usuarios > urls.py
+from django.urls import path
+
+from apps.usuarios.views import login, cadastro, logout
+
+urlpatterns = [
+    path('login', login, name='login'),
+    path('cadastro', cadastro, name='cadastro'),
+    path('logout', logout, name='logout'),
+]
